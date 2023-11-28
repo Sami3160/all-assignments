ddf8d16aaeadf0939f22e8cd8e814f13bc1e7a80
Sami3160 <samibhadgaonkar@gmail.com>
2023-11-29 00:30:34 +0530

    Anagram function answer

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -8,7 +8,17 @@
 */
 
 function isAnagram(str1, str2) {
+ let f =1;
 
+ for(let i=0;i<str1.length;i++){
+   if(str2.indexOf(str1[i])==-1)
+   {
+    f=0
+    break;
+   }
+ }
+ return (f==1?true:false)
 }
 
-module.exports = isAnagram;
+console.log(isAnagram('sam','msa'))
+// module.exports = isAnagram;
